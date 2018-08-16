85882f69717d80a05be658a578487d51417a0045
Bastien Caudan <bastien.caudan@gmail.com>
2018-08-16 22:34:29 +0200

    Use new algolia account

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,13 @@
 const config = {
   port: process.env.PORT || 4000,
-  indexName: 'appstore-search',
-  applicationId: 'EYVD6N6UFE',
+  indexName: 'prod_appstore-search-desc',
+  applicationId: 'BTXKPPT0IV',
   adminKey: process.env.ALGOLIA_ADMIN_KEY
 };
 
 if (process.env.NODE_ENV === 'TEST') {
   config.port = 1234;
-  config.indexName = 'test-index';
+  config.indexName = 'test_appstore-search';
 }
 
 module.exports = config;
--- frontend/src/database/config.js
+++ frontend/src/database/config.js
@@ -1,9 +1,9 @@
 const config = {
-  applicationID: 'EYVD6N6UFE',
-  apiKey: '7287a3dbe0d1cb5879f60deb0586ac6f',
+  applicationID: 'BTXKPPT0IV',
+  apiKey: '2e38efda898845a71085f328555eb349',
   indexes: {
-    desc: 'appstore-search',
-    asc: 'appstore-search-rank-asc',
+    desc: 'prod_appstore-search-desc',
+    asc: 'prod_appstore-search-asc',
   },
   facet: 'category',
 };
