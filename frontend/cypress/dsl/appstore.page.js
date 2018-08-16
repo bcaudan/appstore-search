312bc2f6a97e849806efec0b7ca2d382e37f0a95
Bastien Caudan <bastien.caudan@gmail.com>
2018-08-16 12:32:19 +0200

    Add scenarios

---
+++ frontend/cypress/dsl/appstore.page.js
@@ -0,0 +1,35 @@
+export default class Page {
+  open() {
+    cy.visit('http://localhost:5000');
+    this.waitApplicationsLoaded();
+  }
+
+  waitApplicationsLoaded() {
+    cy.get('.applications .application').should('be.visible');
+    cy.get('.loading.visible').should('not.be.visible');
+  }
+
+  getApplications() {
+    return cy.get('.applications .application');
+  }
+
+  search(candidate) {
+    cy.get('.search input').type(candidate);
+    this.waitApplicationsLoaded();
+  }
+
+  sort(type) {
+    cy.get('.sort').click();
+    cy.get(`[data-sort="${type}"]`).should('be.visible');
+    cy.get(`[data-sort="${type}"]`).click();
+    cy.get(`[data-sort="${type}"]`).should('not.be.visible');
+    this.waitApplicationsLoaded();
+  }
+
+  selectCategory(name) {
+    cy.get('.category-toggle').click();
+    cy.get('.category').contains(name).click();
+    cy.get('.category').should('not.be.visible');
+    this.waitApplicationsLoaded();
+  }
+}
